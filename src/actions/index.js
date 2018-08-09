9896c43bdc69b83bca411c00ccaf4c30cfea3f45
DaironSib <daironsib@gmail.com>
2018-08-09 23:03:40 +0200

    upload

---
+++ src/actions/index.js
@@ -0,0 +1,12 @@
+export {
+    ADD_TODO,
+  DELETE_TODO,
+  TOGGLE_TODO,
+  EDIT_TODO,
+  addTodo,
+  deleteTodo,
+  toggleTodo,
+  editTodo
+} from './todo';
+
+export { SET_FILTER, setFilter } from './filter'
